7f784a0b45bd227745ad7dfe12de1f139fcf76b6
Manuel Perez <mperez@yopapp.me>
2020-11-16 09:15:56 -0300

    Configure dependencies --- Is planned to use a dynamodb table as datasource, also is included a dependency to `poc/model` module.

--- poc/datasource/build.gradle.kts
+++ poc/datasource/build.gradle.kts
@@ -5,7 +5,20 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
+    implementation(project(":poc:model"))
+
+    implementation(platform("software.amazon.awssdk:bom:2.15.21"))
+    implementation("software.amazon.awssdk:dynamodb") {
+        exclude("software.amazon.awssdk", "apache-client")
+        exclude("software.amazon.awssdk", "netty-nio-client")
+    }
+    implementation("software.amazon.awssdk:url-connection-client")
+
     testImplementation(kotlin("test-junit"))
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+    testImplementation("org.testcontainers:localstack:1.14.3")
+
+    testRuntimeOnly("com.amazonaws:aws-java-sdk-dynamodb:1.11.689")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
